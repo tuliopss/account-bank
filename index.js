f78735c2723ab39125df1dbaa5c142082490770d
unknown <tuliovictorp@hotmail.com>
2023-06-28 12:41:50 -0300

    get saldo

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function operation() {
           break;
 
         case "Consultar saldo":
-          //to do
+          getAccountBalance();
           break;
 
         case "Depositar":
@@ -168,4 +168,26 @@ function getAccount(accountName) {
   });
 
   return JSON.parse(accountJSON);
+}
+
+function getAccountBalance() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Qual o nome da sua conta?",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      if (!checkAccount(accountName)) {
+        return getAccountBalance();
+      }
+
+      const accountData = getAccount(accountName);
+      console.log(chalk.bgBlue.black(`Saldo de: ${accountData.balance}`));
+      operation();
+    })
+    .catch();
 }
